817c43e0dc1fb9c6322ba6fb692440603bb4ee7d
syihwamoza <mozakastella26@gmail.com>
2025-11-20 19:23:20 +0700

    Menambahkan daftar jenis kelamin pada objek DataJK

--- app/src/main/java/com/example/myarsitektur/model/DataJK.kt
+++ app/src/main/java/com/example/myarsitektur/model/DataJK.kt
@@ -1,11 +1,5 @@
 package com.example.myarsitektur.model
 
-import com.example.myarsitektur.R;
-
 object DataJK {
     val JenisK= listOf(
-        R.string.Laki,
-        R.string.wanita
-    )
 
-}
