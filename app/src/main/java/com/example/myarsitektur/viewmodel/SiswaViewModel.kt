44c9f4405c217612f283bc925ca38ef098af529b
syihwamoza <mozakastella26@gmail.com>
2025-11-20 19:36:14 +0700

    Menambahkan SiswaViewModel dengan MutableStateFlow untuk status UI siswa

--- app/src/main/java/com/example/myarsitektur/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/myarsitektur/viewmodel/SiswaViewModel.kt
@@ -10,10 +10,4 @@ import kotlinx.coroutines.flow.update
 
 class SiswaViewModel: ViewModel() {
     private val _statusUI = MutableStateFlow(Siswa())
-    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
-    fun setSiswa(ls: MutableList<String>){
-        _statusUI.update{ statusSaatIni ->
-            statusSaatIni.copy(nama=ls[0], gender=ls[1], alamat=ls[2])
-        }
-    }
 }
