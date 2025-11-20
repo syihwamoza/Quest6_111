dc158c1ea455b63c388b985d36aaa892e3bf373e
syihwamoza <mozakastella26@gmail.com>
2025-11-20 19:34:36 +0700

    Menambahkan UI TampilSiswa untuk menampilkan detail nama, gender, dan alamat siswa

--- app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/TampilSiswa.kt
@@ -33,3 +33,42 @@ fun TampilSiswa(
     statusUISiswa: Siswa,
     onBackButtonClicked:()-> Unit
 )
+{
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama), statusUISiswa.nama),
+        Pair(stringResource(id = R.string.gender), statusUISiswa.gender),
+        Pair(stringResource(id = R.string.alamat),statusUISiswa.alamat),
+    )
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id=R.string.detail) ,
+                    color = Color.White)},
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(colorResource
+                        (id = R.color.purple_500)
+                    ))
+        }) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource
+                    (id = R.dimen.padding_small)
+                )
+            ) {
+                items.forEach { item ->
+                    Column {
+                        Text(
+                            text = item.first.uppercase(),
+                            fontSize = 16.sp
+                        )
+                        Text(
+                            text = item.second,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.Cursive, fontSize = 22.sp
+                        )
+                    }
