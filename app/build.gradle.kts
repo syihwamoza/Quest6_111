c357f03c3c1e7d0d57385b790586871aaa4f15f4
syihwamoza <mozakastella26@gmail.com>
2025-11-20 19:48:34 +0700

    Integrasi ViewModel Compose, Runtime Compose, dan Navigation Compose ke project.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
     implementation(libs.navigation.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
